6e5990696c2444385e8311bf875cb00e2026c45b
Mathias Scherer <mathias@aragon.org>
2021-11-11 14:46:07 +0100

    improves generate-instance script with coping assets (#34)

--- scripts/generate-instance.sh
+++ scripts/generate-instance.sh
@@ -9,9 +9,17 @@ cd $TEMP_DIR
 git clone "https://arabot-1:$SOURCECRED_GITHUB_TOKEN@github.com/aragon/Aracred.git"
 cd Aracred
 git checkout gh-pages
-rm -rf /app/site/{output,data,config,sourcecred.json}
-cp -r /app/{output,data,config,sourcecred.json,package.json,yarn.lock} /app/site/
-cp -r /app/site/* ./ 
+rm -rf /app/site/output
+rm -rf /app/site/data
+rm -rf /app/site/config
+rm -rf /app/site/sourcecred.json
+cp -rf /app/output /app/site/
+cp -rf /app/data /app/site/
+cp -rf /app/config /app/site/
+cp -rf /app/sourcecred.json /app/site/
+cp -rf /app/package.json /app/site/
+cp -rf /app/yarn.lock /app/site/
+cp -rf /app/site/* ./ 
 
 git config --global user.email "arabot-1@aragon.org"
 git config --global user.name "Arabot-1"
