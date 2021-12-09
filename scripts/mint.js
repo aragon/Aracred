90e7689dcb06c975ad49b1f7df437eddee3a5f86
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2021-12-09 13:04:34 +0100

    Cleaning of the mint script.

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,36 +4,42 @@ const fs = require("fs-extra");
 const Ledger = sc.ledger.ledger.Ledger;
 const G = sc.ledger.grain;
 
-const LEDGER_PATH = 'data/ledger.json';
-const MINT_AMOUNTS_PATH = './scripts/toMint01Merkle.json';
+const LEDGER_PATH = './../data/ledger.json';
+const addresses = require("./../payments/addresses.json");
 
-const addresses = require("./addresses.json");
+const PAYMENT_ID = "01";
 
 (async function () {
-  const ledgerJSON = (await fs.readFile(LEDGER_PATH)).toString();
-  const ledger = Ledger.parse(ledgerJSON);
-  const accounts = ledger.accounts();
+    const ledgerJSON = (await fs.readFile(LEDGER_PATH)).toString();
+    const ledger = Ledger.parse(ledgerJSON);
+    const accounts = ledger.accounts();
 
-  const newMintAmounts = {};
-  let total = 0;
-  accounts.forEach(acc => {
-    const amountToMint = G.format(acc.balance, 9, '');
-    if (amountToMint > 0.0) {
+    const payments = {};
+    let total = BigInt(0);
+    accounts.forEach(acc => {
 
-      name = acc.identity.name
-      if (name in addresses) {
-        newMintAmounts[addresses[acc.identity.name]] = amountToMint;
-        total += parseFloat(amountToMint);
-      } else
-        throw `Name "${name}" not found in addresses list.`;
-    }
-  });
+        const amountToMint = BigInt(acc.balance)
+        if (amountToMint > 0.0) {
+            console.log(BigInt(acc.balance))
+            name = acc.identity.name
+            if (name in addresses) {
+                payments[addresses[acc.identity.name]] = amountToMint.toString();
+                total += amountToMint;
+            } else
+                throw `Name "${name}" not found in addresses list.`;
+        }
+    });
 
-  console.log(Object.entries(newMintAmounts).map(([name, amount]) => {
+    console.log(`Total: ${total} (${total / BigInt(10 ** 18)} ANT)`);
 
-    return `${name},${amount}`
-  }).join('\n'));
-  console.log({ total });
-  
-  fs.writeFile(MINT_AMOUNTS_PATH, JSON.stringify(newMintAmounts));
+    fs.writeFile(`./../payments/payment_${PAYMENT_ID}.json`,
+        JSON.stringify({
+            "title": `Ambassador SourceCred Payment #${Number(PAYMENT_ID)}`,
+            "justification":
+                `Payment of SourceCred rewards to the ambassadors for their activities in the AN DAO amounting to ${total / BigInt(10 ** 18)} ANT for the weeks 38 to 47.`,
+            "externalContract" : "0xD152f549545093347A162Dce210e7293f1452150", // Disperse.app on mainnet
+            "token": "0xa117000000f279d81a1d3cc75430faa017fa5a2e", // ANT token
+            "recipients": Object.keys(payments),
+            "values": Object.values(payments)
+        }, null, 2));
 })();
