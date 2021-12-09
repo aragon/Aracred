df8e70164adb7083fe813a6b9d7948f5fb5b78b3
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2021-12-10 00:00:58 +0100

    Finished script to calculated payments.

--- scripts/calculatePayments.js
+++ scripts/calculatePayments.js
@@ -1,20 +1,39 @@
 const sc = require('sourcecred').sourcecred;
-const fs = require("fs-extra");
-
-const Ledger = sc.ledger.ledger.Ledger;
-const G = sc.ledger.grain;
+const fs = require("fs");
 
 const LEDGER_PATH = './../data/ledger.json';
+const Ledger = sc.ledger.ledger.Ledger;
+const ledger = Ledger.parse(fs.readFileSync(LEDGER_PATH, 'utf8'));
+
 const ANT_TOKEN_ADDRESS = "0xa117000000f279d81a1d3cc75430faa017fa5a2e";
 const DISPERSE_CONTRACT_ADDRESS = "0xD152f549545093347A162Dce210e7293f1452150";
 const PAYMENT_ID = "01";
 
 
-(async function () {
-    const ledgerJSON = (await fs.readFile(LEDGER_PATH)).toString();
-    const ledger = Ledger.parse(ledgerJSON);
-    const accounts = ledger.accounts();
+function writePaymentsFile(total, recipients, values, sourcecredIds, paymentId) {
+    const filename = `./../payments/payment_${paymentId}.json`
 
+    if (!fs.existsSync(filename)) {
+        fs.writeFileSync(filename,
+            JSON.stringify({
+                "title": `Ambassador SourceCred Payment #${Number(paymentId)}`,
+                "justification":
+                    `Payment of SourceCred rewards to the ambassadors for their activities in the AN DAO amounting to ${Number(total) / 10 ** 18} ANT for the weeks 38 to 47.`,
+                "externalContract": DISPERSE_CONTRACT_ADDRESS,
+                "token": ANT_TOKEN_ADDRESS,
+                "recipients": recipients,
+                "values": values,
+                "_sourcecredIds": sourcecredIds,
+                "_balancesUpdated": false
+            }, null, 2)
+        );
+    } else {
+        throw Error('File exists already');
+    }
+}
+
+function calculatePayments() {
+    const accounts = ledger.accounts();
     const recipients = [];
     const values = [];
     const sourcecredIds = [];
@@ -42,33 +61,10 @@ const PAYMENT_ID = "01";
                 console.log(addr, amountToPay.toString());
             }
         }
-
     });
-    
-    console.log(`Total: ${total} (${Number(total) / 10 ** 18} ANT)`);
 
     writePaymentsFile(total, recipients, values, sourcecredIds, PAYMENT_ID);
-})();
+    console.log(`Total: ${total} (${Number(total) / 10 ** 18} ANT)`);
+};
 
-
-function writePaymentsFile(total, recipients, values, sourcecredIds, paymentId) {
-    const filename = `./../payments/payment_${paymentId}.json`
-
-    if (!fs.existsSync(filename)) {
-        fs.writeFile(filename,
-            JSON.stringify({
-                "title": `Ambassador SourceCred Payment #${Number(paymentId)}`,
-                "justification":
-                    `Payment of SourceCred rewards to the ambassadors for their activities in the AN DAO amounting to ${Number(total) / 10 ** 18} ANT for the weeks 38 to 47.`,
-                "externalContract": DISPERSE_CONTRACT_ADDRESS,
-                "token": ANT_TOKEN_ADDRESS,
-                "recipients": recipients,
-                "values": values,
-                "_sourcecredIds": sourcecredIds,
-                "_balancesUpdated" : false
-            }, null, 2)
-        );
-    } else {
-        throw Error('File exists already');
-    }
-}
+calculatePayments();
